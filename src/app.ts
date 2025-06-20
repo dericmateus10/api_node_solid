8aefc4359023fadd59e017845fd51245bf6904a4
Deric Mateus <dericmateus10@gmail.com>
2025-06-19 22:52:43 -0300

    Add Prisma setup with user, gym, and check-in models; configure PostgreSQL in Docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastity from 'fastify'
+import { PrismaClient } from 'generated/prisma'
 
 export const app = fastity()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Deric Mateus',
+    email: 'dericmateus10@gmail.com',
+  },
+})
