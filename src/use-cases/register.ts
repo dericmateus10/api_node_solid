c1020a2156518bf5275ecf54592185979a177a7c
Deric Mateus <dericmateus10@gmail.com>
2025-06-23 12:06:47 -0300

    Implement user registration feature with Prisma and bcrypt; update ESLint configuration and package dependencies

---
+++ src/use-cases/register.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor( private usersRepository: any) {}
+
+    async execute({
+    name,
+    email,
+    password,
+}: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error("User already exists with this email")
+  }
+
+
+  await this.usersRepository.create({
+    name,
+    email,
+    password_hash,
+  })
+
+}
+}
+
